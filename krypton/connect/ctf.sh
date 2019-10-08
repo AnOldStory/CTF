cd77f48449ad6cfb9e7cf81ce07b8e7ae4f8fe42
HyeongChang Lee <hc9904@hanyang.ac.kr>
2019-10-08 17:04:16 +0900

    Update krypton

---
+++ krypton/connect/ctf.sh
@@ -0,0 +1,19 @@
+echo 단계를 입력하시오. 
+read STEP
+
+arr=(`cat password`)
+arr_leng=${#arr[@]}
+
+if [ $STEP -gt $(($arr_leng - 1)) ]; then
+	echo 새로운 단계의 비밀번호를 입력하시오.
+	read PASSWORD
+	echo $PASSWORD >> password 
+	echo $arr_leng 번째 암호가 입력되었습니다!
+else
+	PASSWORD=${arr[$STEP]}
+fi
+
+echo 단계: $STEP 비밀번호: $PASSWORD
+echo 접속을 시도합니다. 
+
+sshpass -p "$PASSWORD" ssh -p 2222 krypton$STEP@krypton.labs.overthewire.org
